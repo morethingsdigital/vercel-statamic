22261027f91b8f861d13904739e9f53ab74e3796
Christoph Stockinger <christoph@morethings.digital>
2023-07-24 17:06:59 +0200

    feat(dto): extend pagination dto

--- src/Dtos/Vercel/Common/VercelPaginationDto.php
+++ src/Dtos/Vercel/Common/VercelPaginationDto.php
@@ -14,7 +14,24 @@ class VercelPaginationDto extends Data
         #[MapName('next')]
         public ?int $next = null,
         #[MapName('prev')]
-        public ?int $prev = null
+        public ?int $prev = null,
+        public int $limit = 10
     ) {
+    }
+
+    public function currentPage() {
+        return floor($this->count / $this->limit);
+    }
+
+    public function nextPage() {
+        if($this->next) return $this->currentPage() + 1;
+
+        return null;
+    }
+
+    public function lastPage() {
+        if($this->prev) return $this->currentPage() - 1;
+
+        return null;
     }
 }
